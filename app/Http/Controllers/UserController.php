3eac128d98e993de16480391d8d83acc02688f66
Alfin Aswar <alfin7aswar@gmail.com>
2024-12-23 15:59:07 +0700

    users dan role

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,7 +1,7 @@
 <?php
-    
+
 namespace App\Http\Controllers;
-    
+
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\User;
@@ -11,7 +11,7 @@ use Hash;
 use Illuminate\Support\Arr;
 use Illuminate\View\View;
 use Illuminate\Http\RedirectResponse;
-    
+
 class UserController extends Controller
 {
     /**
@@ -21,11 +21,12 @@ class UserController extends Controller
      */
     public function index(Request $request): View
     {
-        $data = User::orderBy('id','DESC')->paginate(5);
-        return view('users.index',compact('data'))
+        $roles = Role::pluck('name', 'name')->all();
+        $data = User::orderBy('id', 'DESC')->get();
+        return view('users.index', compact('data', 'roles'))
             ->with('i', ($request->input('page', 1) - 1) * 5);
     }
-    
+
     /**
      * Show the form for creating a new resource.
      *
@@ -33,10 +34,10 @@ class UserController extends Controller
      */
     public function create(): View
     {
-        $roles = Role::pluck('name','name')->all();
-        return view('users.create',compact('roles'));
+        $roles = Role::pluck('name', 'name')->all();
+        return view('users.create', compact('roles'));
     }
-    
+
     /**
      * Store a newly created resource in storage.
      *
@@ -45,23 +46,32 @@ class UserController extends Controller
      */
     public function store(Request $request): RedirectResponse
     {
-        $this->validate($request, [
-            'name' => 'required',
-            'email' => 'required|email|unique:users,email',
-            'password' => 'required|same:confirm-password',
-            'roles' => 'required'
-        ]);
-    
+
         $input = $request->all();
+        // dd($request->hasFile('Foto'));
+
+        if ($request->hasFile('Foto')) {
+            $file = $request->file('Foto');
+            $file->storeAs('public/Foto', $file->getClientOriginalName());
+            $data['Foto'] = $file->getClientOriginalName();
+        }
+        if ($request->hasFile('DigitalSign')) {
+            $file = $request->file('DigitalSign');
+            $file->storeAs('public/DigitalSign', $file->getClientOriginalName());
+            $data['DigitalSign'] = $file->getClientOriginalName();
+        }
+
+
+        // dd($input);
         $input['password'] = Hash::make($input['password']);
-    
+        $input['akses'] = $request->roles[0];
+
         $user = User::create($input);
         $user->assignRole($request->input('roles'));
-    
-        return redirect()->route('users.index')
-                        ->with('success','User created successfully');
+
+        return redirect()->route('users.index')->with('success', 'User created successfully');
     }
-    
+
     /**
      * Display the specified resource.
      *
@@ -70,10 +80,9 @@ class UserController extends Controller
      */
     public function show($id): View
     {
-        $user = User::find($id);
-        return view('users.show',compact('user'));
+
     }
-    
+
     /**
      * Show the form for editing the specified resource.
      *
@@ -83,12 +92,12 @@ class UserController extends Controller
     public function edit($id): View
     {
         $user = User::find($id);
-        $roles = Role::pluck('name','name')->all();
-        $userRole = $user->roles->pluck('name','name')->all();
-    
-        return view('users.edit',compact('user','roles','userRole'));
+        $roles = Role::pluck('name', 'name')->all();
+        $userRole = $user->roles->pluck('name', 'name')->all();
+
+        return view('users.edit', compact('user', 'roles', 'userRole'));
     }
-    
+
     /**
      * Update the specified resource in storage.
      *
@@ -99,29 +108,46 @@ class UserController extends Controller
     public function update(Request $request, $id): RedirectResponse
     {
         $this->validate($request, [
-            'name' => 'required',
-            'email' => 'required|email|unique:users,email,'.$id,
-            'password' => 'same:confirm-password',
-            'roles' => 'required'
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email,' . $id,
+            'nip' => 'required|string|unique:users,nip,' . $id,
+            'department' => 'required|string|max:255',
+            'jabatan' => 'required|string|max:255',
+            'hp' => 'required|string|max:20',
+            'alamat' => 'required|string',
+            'roles' => 'required|array',
+            'password' => 'nullable|min:6|same:confirm-password',
+            'Foto' => 'nullable|image|mimes:jpeg,png,jpg|max:2048',
+            'DigitalSign' => 'nullable|image|mimes:jpeg,png,jpg|max:2048'
         ]);
-    
+
         $input = $request->all();
-        if(!empty($input['password'])){ 
-            $input['password'] = Hash::make($input['password']);
-        }else{
-            $input = Arr::except($input,array('password'));    
+        if ($request->hasFile('Foto')) {
+            $file = $request->file('Foto');
+            $file->storeAs('public/Foto', $file->getClientOriginalName());
+            $input['Foto'] = $file->getClientOriginalName();
         }
-    
+        if ($request->hasFile('DigitalSign')) {
+            $file = $request->file('DigitalSign');
+            $file->storeAs('public/DigitalSign', $file->getClientOriginalName());
+            $input['DigitalSign'] = $file->getClientOriginalName();
+        }
+        if (!empty($input['password'])) {
+            $input['password'] = Hash::make($input['password']);
+        } else {
+            $input = Arr::except($input, array('password'));
+        }
+
         $user = User::find($id);
         $user->update($input);
-        DB::table('model_has_roles')->where('model_id',$id)->delete();
-    
+        DB::table('model_has_roles')->where('model_id', $id)->delete();
+
         $user->assignRole($request->input('roles'));
-    
-        return redirect()->route('users.index')
-                        ->with('success','User updated successfully');
+
+        return redirect()->back()
+            ->with('success', 'User updated successfully');
     }
-    
+
     /**
      * Remove the specified resource from storage.
      *
@@ -132,6 +158,6 @@ class UserController extends Controller
     {
         User::find($id)->delete();
         return redirect()->route('users.index')
-                        ->with('success','User deleted successfully');
+            ->with('success', 'User deleted successfully');
     }
 }
--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -1,69 +1,137 @@
 @extends('layouts.app')
 
-
 @section('content')
-<div class="row">
-    <div class="col-lg-12 margin-tb">
-        <div class="pull-left">
-            <h2>Edit New User</h2>
+    @if ($message = Session::get('success'))
+        <script>
+            Swal.fire({
+                icon: 'success',
+                title: 'success',
+                text: '{{ $message }}',
+            });
+        </script>
+    @endif
+
+    @if (count($errors) > 0)
+        <div class="alert alert-danger">
+            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
         </div>
-        <div class="pull-right">
-            <a class="btn btn-primary" href="{{ route('users.index') }}"> Back</a>
-        </div>
-    </div>
-</div>
+    @endif
+
+    @php
+        $formRoute = isset($user) ? ['users.update', $user->id] : 'users.store';
+        $formMethod = isset($user) ? 'PATCH' : 'POST';
+    @endphp
+
+    {!! Form::model($user ?? null, [
+        'route' => $formRoute,
+        'method' => $formMethod,
+        'class' => 'profile-form',
+        'enctype' => 'multipart/form-data',
+    ]) !!}
+    <div class="row">
 
 
-@if (count($errors) > 0)
-  <div class="alert alert-danger">
-    <strong>Whoops!</strong> There were some problems with your input.<br><br>
-    <ul>
-       @foreach ($errors->all() as $error)
-         <li>{{ $error }}</li>
-       @endforeach
-    </ul>
-  </div>
-@endif
+        <div class="col-xl-10 col-lg-8">
+            <div class="card card-bx m-b30">
+                <div class="card-header">
+                    <h6 class="title">Data Karyawan</h6>
+                </div>
 
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-sm-6 m-b30">
+                            <label class="form-label">Name</label>
+                            {!! Form::text('name', null, ['placeholder' => 'Nama', 'class' => 'form-control']) !!}
+                        </div>
+                        <div class="col-sm-6 m-b30">
+                            <label class="form-label">Email</label>
+                            {!! Form::text('email', null, ['placeholder' => 'Email', 'class' => 'form-control']) !!}
+                        </div>
+                        <div class="col-sm-6 m-b30">
+                            <label class="form-label">Nomor Identitas Pegawai</label>
+                            {!! Form::text('nip', null, ['placeholder' => 'Nomor Identitas Pegawai', 'class' => 'form-control']) !!}
+                        </div>
 
-{!! Form::model($user, ['method' => 'PATCH','route' => ['users.update', $user->id]]) !!}
-<div class="row">
-    <div class="col-xs-12 col-sm-12 col-md-12">
-        <div class="form-group">
-            <strong>Name:</strong>
-            {!! Form::text('name', null, array('placeholder' => 'Name','class' => 'form-control')) !!}
-        </div>
-    </div>
-    <div class="col-xs-12 col-sm-12 col-md-12">
-        <div class="form-group">
-            <strong>Email:</strong>
-            {!! Form::text('email', null, array('placeholder' => 'Email','class' => 'form-control')) !!}
-        </div>
-    </div>
-    <div class="col-xs-12 col-sm-12 col-md-12">
-        <div class="form-group">
-            <strong>Password:</strong>
-            {!! Form::password('password', array('placeholder' => 'Password','class' => 'form-control')) !!}
-        </div>
-    </div>
-    <div class="col-xs-12 col-sm-12 col-md-12">
-        <div class="form-group">
-            <strong>Confirm Password:</strong>
-            {!! Form::password('confirm-password', array('placeholder' => 'Confirm Password','class' => 'form-control')) !!}
-        </div>
-    </div>
-    <div class="col-xs-12 col-sm-12 col-md-12">
-        <div class="form-group">
-            <strong>Role:</strong>
-            {!! Form::select('roles[]', $roles,$userRole, array('class' => 'form-control','multiple')) !!}
-        </div>
-    </div>
-    <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </div>
-</div>
-{!! Form::close() !!}
+                        <div class="col-sm-6 m-b30">
+                            <label class="form-label">Role</label>
+                            {!! Form::select('roles[]', $roles, isset($userRole) ? $userRole : [], [
+                                'class' => 'me-sm-2 default-select form-control wide',
+                            ]) !!}
+                        </div>
+                        <div class="col-sm-6 m-b30">
+                            <label class="form-label">Departemen</label>
+                            {!! Form::text('department', null, ['placeholder' => 'Departmen', 'class' => 'form-control']) !!}
+                        </div>
+                        <div class="col-sm-6 m-b30">
+                            <label class="form-label">Jabatan</label>
+                            {!! Form::text('jabatan', null, ['placeholder' => 'Jabatan', 'class' => 'form-control']) !!}
+                        </div>
+                        <div class="col-sm-6 m-b30">
+                            <label class="form-label">Nomor HP</label>
+                            {!! Form::number('hp', null, ['placeholder' => 'Nomor HP', 'class' => 'form-control']) !!}
+                        </div>
+                        <div class="col-sm-6 m-b30">
+                            <label class="form-label">Alamat</label>
+                            {!! Form::textarea('alamat', null, ['placeholder' => 'Alamat', 'class' => 'form-control', 'rows' => 3]) !!}
+                        </div>
 
+                        <div class="col-sm-6 m-b30">
+                            <label class="form-label">Password</label>
+                            {!! Form::password('password', ['placeholder' => 'Password', 'class' => 'form-control']) !!}
+                            @if (isset($user))
+                                <small class="text-muted">Leave blank if you don't want to change the password</small>
+                            @endif
+                        </div>
+                        <div class="col-sm-6 m-b30">
+                            <label class="form-label">Confirm Password</label>
+                            {!! Form::password('confirm-password', ['placeholder' => 'Confirm Password', 'class' => 'form-control']) !!}
+                        </div>
+                        <div class="col-sm-6 m-b30">
+                            <label class="form-label">Foto</label>
+                            <input type="file" name="Foto" class="form-control" id="profileImageInput"
+                                accept="image/*">
+                            @if (isset($user) && $user->Foto)
+                                <div class="mt-2">
+                                    <img src="{{ asset('storage/Foto/' . $user->Foto) }}" alt="Current Profile Photo"
+                                        class="img-thumbnail" style="max-width: 150px">
+                                </div>
+                            @endif
+                        </div>
+                        <div class="col-sm-6 m-b30">
+                            <label class="form-label">Tanda Tangan</label>
+                            <input type="file" name="DigitalSign" class="form-control" accept="image/*">
+                            @if (isset($user) && $user->DigitalSign)
+                                <div class="mt-2">
+                                    <img src="{{ asset('storage/DigitalSign/' . $user->DigitalSign) }}"
+                                        alt="Current Digital Signature" class="img-thumbnail" style="max-width: 150px">
+                                </div>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <button type="submit" class="btn btn-primary">{{ isset($user) ? 'Update' : 'Simpan' }}</button>
+                </div>
+                {!! Form::close() !!}
+            </div>
+        </div>
+    </div>
 
-<p class="text-center text-primary"><small>Tutorial by ItSolutionStuff.com</small></p>
+    <script>
+        document.getElementById('profileImageInput').addEventListener('change', function(event) {
+            const input = event.target;
+            if (input.files && input.files[0]) {
+                const reader = new FileReader();
+                reader.onload = function(e) {
+                    document.getElementById('profileImage').src = e.target.result;
+                }
+                reader.readAsDataURL(input.files[0]);
+            }
+        });
+    </script>
 @endsection
